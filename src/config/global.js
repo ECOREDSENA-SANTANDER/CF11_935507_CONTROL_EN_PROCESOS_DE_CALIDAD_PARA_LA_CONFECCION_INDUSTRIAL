071892b59bba7dde5668ea18788cf5c191e4ffb4
Andrea Botello <abotellod@gmail.com>
2026-09-11 18:13:01 -0500

    CFA

--- src/config/global.js
+++ src/config/global.js
@@ -161,7 +161,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/935507_CF11_CFA.pdf',
       },
       {
         icono: 'fas fa-download',
